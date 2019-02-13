e6428a555dddcd8b294c7bbcd4b70e86c93789a2
ZhibinHuang <43126280+ZhibinHuang@users.noreply.github.com>
2019-02-13 13:58:44 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='heart_monitor',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='ZhibinHuang',
+    author_email='hzhibin@bu.edu',
+    description='Mocks behavior of a heart monitor'
+)
